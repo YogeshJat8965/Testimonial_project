dcd51b500d1f75f2a8dda48b6a07b5f51af6f503
YogeshJat8965 <yogeshjat8965@gmail.com>
2024-08-03 06:02:30 +0530

    Ready for deployement

--- src/Card.js
+++ src/Card.js
@@ -1,39 +1,32 @@
 import React from "react";
-import './Card.css';
-import { FaQuoteLeft,FaQuoteRight } from "react-icons/fa6";
+import "./Card.css";
+import { FaQuoteLeft, FaQuoteRight } from "react-icons/fa6";
 
+export default function Card({ review }) {
+  return (
+    <div className="flex flex-col md:relative   ">
+      <div className=" top-[3rem] mx-auto left-[8.5rem]  ">
+        <img
+          className="aspect-square rounded-full w-[140px] h-[140px]   "
+          src={review.image}
+        ></img>
+        <div className="w-[140px] h-[140px] rounded-full bg-violet-500 rounded-full absolute top-[-6px] z-[-10] left-[10px] "></div>
+      </div>
 
-export default function Card({review}){
+      <div className="text-center mt-7">
+        <p className="font-bold text-2xl capitalize">{review.name}</p>
+      </div>
 
-    return (
-
-        <div className="flex flex-col md:relative   ">
-
-            <div className="absolute top-[-7rem] z-[10] mx-auto " >
-                <img className="aspect-square rounded-full w-[140px] h-[140px] z-25  "
-                src={review.image} ></img>
-                <div className="w-[140px] h-[140px] rounded-full bg-violet-500 rounded-full absolute top-[-6px] z-[-10] left-[10px] " ></div>
-            </div>
-
-            <div className="text-center mt-7" >
-                <p className="font-bold text-2xl capitalize" >{review.name}</p>
-            </div>
-
-            <div className="text-center mt-7" >
-                <p className="text-violet-400 uppercase text-md" >{review.job}</p>
-            </div>
-            <div className="text-violet-400 mx-auto mt-5" >
-                <FaQuoteLeft></FaQuoteLeft>
-            </div>
-            <div className="text-center mt-4 text-slate-500" >
-                {review.text}
-            </div>
-            <div className="text-violet-400 mx-auto mt-5">
-                <FaQuoteRight></FaQuoteRight>
-            </div>
-
-        </div>
-
-    );
-
+      <div className="text-center mt-7">
+        <p className="text-violet-400 uppercase text-md">{review.job}</p>
+      </div>
+      <div className="text-violet-400 mx-auto mt-5">
+        <FaQuoteLeft></FaQuoteLeft>
+      </div>
+      <div className="text-center mx-3 mt-4 text-slate-500">{review.text}</div>
+      <div className="text-violet-400 mx-auto px-[50px] mt-5">
+        <FaQuoteRight></FaQuoteRight>
+      </div>
+    </div>
+  );
 }
--- src/Testimonial.js
+++ src/Testimonial.js
@@ -1,59 +1,50 @@
 import React, { useState } from "react";
-import Card from "./Card.js"
+import Card from "./Card.js";
 import './Testimonial.css';
 import { TbMathGreater } from "react-icons/tb";
 import { LiaLessThanSolid } from "react-icons/lia";
 
-
 export default function Testimonial({ reviews }) {
 
-    const[index,setIndex] = useState(0);
+    const [index, setIndex] = useState(0);
 
-    function backIndex(){
-
-        if(index - 1 < 0){
+    function backIndex() {
+        if (index - 1 < 0) {
             setIndex(4);
-        }
-        else{
-            setIndex(index-1);
-        }
-    }
-    function forwardIndex(){
-        if(index + 1 > 4){
-            setIndex(0);
-        }
-        else{
-            setIndex(index+1);
+        } else {
+            setIndex(index - 1);
         }
     }
 
-    function supriseHandler(){
+    function forwardIndex() {
+        if (index + 1 > 4) {
+            setIndex(0);
+        } else {
+            setIndex(index + 1);
+        }
+    }
+
+    function surpriseHandler() {
         let randmIndex = Math.floor(Math.random() * reviews.length);
         setIndex(randmIndex);
     }
 
     return (
-        <div className="w-[85vw] md:w-[700px] bg-white flex flex-col justify-center items-center mt-10 p-10
-         transition-all duration-500 hover:shadow-2xl rounded-lg " >
-           <Card review={reviews[index]} ></Card> 
-            <div className="flex text-3xl mt-5 gap-3 text-violet-400 font-bold mx-auto  " >
-                <button className="cursor-pointer hover:text-violet-800" onClick={backIndex} >
-                    <LiaLessThanSolid></LiaLessThanSolid>
+        <div className="testimonial-container">
+            <Card review={reviews[index]} />
+            <div className="testimonial-controls">
+                <button onClick={backIndex}>
+                    <LiaLessThanSolid />
                 </button>
-                <button className="cursor-pointer hover:text-violet-800" onClick={forwardIndex} >
-                    <TbMathGreater></TbMathGreater>
-                </button>
-            </div>
-            <div className="mt-6" >
-                <button className="bg-violet-400 hover:bg-violet-600 transition-all duration-200 cursor-pointer 
-                    px-10 py-2 rounded-md font-bold text-white text-lg" onClick={supriseHandler} >
-                    Suprise Me
+                <button onClick={forwardIndex}>
+                    <TbMathGreater />
                 </button>
             </div>
-
-
-
+            <div>
+                <button className="surprise-button" onClick={surpriseHandler}>
+                    Surprise Me
+                </button>
+            </div>
         </div>
     );
-
 }
